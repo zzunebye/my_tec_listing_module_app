9e171348371fd30902df2416b2fe71fad6fd7ce0
Jun Young Bang <bhang96@gmail.com>
2025-06-22 21:32:40 +0800

    move wrapped-filter to widgets folder (wrapper-filter currently is dummy UI)

---
+++ lib/widgets/wrapped_filters.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:my_tec_listing_module_app/screens/booking_list_screen.dart';
+import 'package:my_tec_listing_module_app/widgets/filter_bottom_sheet.dart';
+
+class WrappedFilters extends HookWidget {
+  const WrappedFilters({super.key});
+
+  void openFilterDialog(BuildContext context, FilterState filterState) {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      enableDrag: true,
+      useRootNavigator: true,
+      context: context,
+      builder: (context) => SafeArea(
+        child: FilterBottomSheet(
+          filterState: filterState,
+          onApply: (filterState) {},
+          onReset: (filterState) {},
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final filterState = useState(
+      FilterState(
+        capacity: 0,
+        date: DateTime.now(),
+        startTime: DateTime.now(),
+        endTime: DateTime.now(),
+        canVideoConference: false,
+      ),
+    );
+
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Wrap(
+        spacing: 4.0,
+        // runSpacing: 8.0,
+        children: [
+          FilterChip(
+            avatar: Icon(Icons.filter_list, size: 18.0, color: Theme.of(context).colorScheme.onSurface),
+            key: Key('filter_chip_0'),
+            label: Text('Filter'),
+            onSelected: (selected) => openFilterDialog(context, filterState.value),
+          ),
+          FilterChip(
+            avatar: Icon(Icons.today, size: 18.0, color: Theme.of(context).colorScheme.onSurface),
+            key: Key('filter_chip_1'),
+            label: Text('Today'),
+            onSelected: (selected) => openFilterDialog(context, filterState.value),
+          ),
+          FilterChip(
+            avatar: Icon(Icons.access_time, size: 18.0, color: Theme.of(context).colorScheme.onSurface),
+            key: Key('filter_chip_2'),
+            label: Text('06:15 PM - 06:45 PM'),
+            onSelected: (selected) => openFilterDialog(context, filterState.value),
+          ),
+          FilterChip(
+            avatar: Icon(Icons.chair, size: 18.0, color: Theme.of(context).colorScheme.onSurface),
+            key: Key('filter_chip_3'),
+            label: Text('4 Seats'),
+            onSelected: (selected) => openFilterDialog(context, filterState.value),
+          ),
+          FilterChip(
+            avatar: Icon(Icons.location_on_outlined, size: 18.0, color: Theme.of(context).colorScheme.onSurface),
+            key: Key('filter_chip_4'),
+            label: Text('All centres in the City'),
+            onSelected: (selected) => openFilterDialog(context, filterState.value),
+          ),
+        ],
+      ),
+    );
+  }
+}
